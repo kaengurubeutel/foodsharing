75b7261905941d9c6debc499ac97c076a7eb1321
Anja Zieger <anja.zieger1@hs-augsburg.de>
2022-02-02 14:01:07 +0100

    Static settings und css eingebunden

--- foodies2/settings.py
+++ foodies2/settings.py
@@ -107,7 +107,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_DIR = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
