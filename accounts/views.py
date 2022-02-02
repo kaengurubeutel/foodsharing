3e221750f2baecb2b31b2ac0d93631488809ee70
Maxi <maximilian.uhlig@hs-augsburg.de>
2022-02-02 11:12:04 +0100

    init

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
 # Create your views here.
+from feed import models
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = models.Profile
+        exclude = ['user']
+
+def register(request):
+    if request.method == 'POST':
+        userForm = UserCreationForm(request.POST)
+        profileForm = ProfileForm(request.POST)
+        if userForm.is_valid() and profileForm.is_valid():
+            userForm.save()
+            profileForm.save()
+            return redirect('login')
+    else:
+        userForm = UserCreationForm()
+        profileForm = ProfileForm()
+    return render(request, 'registration/register.html', dict(form=userForm, profileForm=profileForm))
+
