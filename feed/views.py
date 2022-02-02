f3c51b4ca08558595eefafb3e6370573b3673001
Maxi <maximilian.uhlig@hs-augsburg.de>
2022-02-02 12:41:02 +0100

    views für upload, feed, löschen usw...

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.forms import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -6,17 +7,19 @@ from feed import models
 from django.contrib.auth.decorators import login_required
 
 from feed.models import Food, Profile
+from foodies2 import settings
 
 
 class FoodForm(forms.ModelForm):
     class Meta:
         model = models.Food
-        exclude = [ 'user']
+        exclude = ['user']
 
+
+# für die folgenenden Views muss man angemeldet sein
 
 @login_required()
 def upload(request):
-
     if request.method == "POST":
         form = FoodForm(request.POST, request.FILES)
         form.instance.user = request.user
@@ -28,24 +31,73 @@ def upload(request):
     return render(request, 'upload.html', dict(form=form))
 
 
-def get_profile_posts(request):
+# Löscht Foodpost und gibt alle anderen zurück
 
+@login_required()
+def delete(request):
+    if request.method == "POST":
+        user_id = request.GET.get('user')
+        search_query = Food.objects.filter(id=request.data.get(id))
+        food_to_delete = search_query[0]
+
+        if food_to_delete.user.id == int(user_id):
+            food_to_delete.delete()
+        else:
+            food_to_delete = []
+        return render(request, 'profile.html', dict(
+            foods=Food.objects.all()
+        ))
+
+
+# gibt daten des Profils und alle Foodposts der Ersteller*in zurück
+@login_required()
+def get_profile_posts(request):
     if request.method == "GET":
         user_id = request.GET.get('user')
         if user_id:
             foods = Food.objects.filter(user=int(user_id))
             profile = Profile.objects.filter(user=int(user_id))
+            user = User.objects.filter(request.get('user'))
         else:
             foods = []
             profile = []
+            user = []
 
         return render(request, 'profile.html', dict(
             foods=foods,
             profile=profile,
+            user=user,
         ))
+
+
+# gibt alle Food posts zurück
+
+@login_required()
 def get_all_posts(request):
     user_id = request.GET.get('user')
-    other_users = User.objects.filter(id != int(user_id))
+    if user_id:
+        foods = models.Food.objects.all()
+        profiles = models.Profile.objects.all()
+        users = models.User.objects.all()
+    else:
+        foods = []
+        profiles = []
+        users = []
+
+    return render(request, 'feed.html', dict(
+        foods=foods,
+        profiles=profiles,
+        users=users,
+    ))
 
 
+# aus der Vorlesung
 
+
+def post_save_receiver(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+post_save.connect(post_save_receiver, sender=settings.AUTH_USER_MODEL)
