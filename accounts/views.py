ff835c81cc96caa0d93ba9954cba707d134c79ae
Maxi <maximilian.uhlig@hs-augsburg.de>
2022-02-02 17:09:19 +0100

    html zeug

--- accounts/views.py
+++ accounts/views.py
@@ -14,13 +14,13 @@ class ProfileForm(forms.ModelForm):
 def register(request):
     if request.method == 'POST':
         userForm = UserCreationForm(request.POST)
-        profileForm = ProfileForm(request.POST)
-        if userForm.is_valid() and profileForm.is_valid():
+        #profileForm = ProfileForm(request.POST)
+        if userForm.is_valid(): #and profileForm.is_valid():
             userForm.save()
-            profileForm.save()
+            #profileForm.save()
             return redirect('login')
     else:
         userForm = UserCreationForm()
-        profileForm = ProfileForm()
-    return render(request, 'auth/register.html', dict(form=userForm, profileForm=profileForm))
+        #profileForm = ProfileForm()
+    return render(request, 'auth/register.html', dict(form=userForm)) #, profileForm=profileForm))
 
