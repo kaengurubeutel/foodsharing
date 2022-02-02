9007044fe83b1ba11fd75dbd4aaa68d448ff6e84
Maxi <maximilian.uhlig@hs-augsburg.de>
2022-02-02 11:32:42 +0100

    init

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,14 @@
+from django.forms import forms
 from django.shortcuts import render
+from accounts import models
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+class FoodForm(forms.ModelForm):
+    class Meta:
+        model = models.Food
+        exclude = [ 'user']
+
+@login_required()
+def feed(request):
+    all
