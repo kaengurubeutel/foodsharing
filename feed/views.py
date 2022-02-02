e14233514625f4d136092f427719ac578f35b601
Maxi <maximilian.uhlig@hs-augsburg.de>
2022-02-02 12:04:38 +0100

    views für upload, feed

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,10 @@
 from django.forms import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from accounts import models
+from feed import models
 from django.contrib.auth.decorators import login_required
+
+from feed.models import Food, Profile
 
 
 class FoodForm(forms.ModelForm):
@@ -9,6 +12,37 @@ class FoodForm(forms.ModelForm):
         model = models.Food
         exclude = [ 'user']
 
+
 @login_required()
-def feed(request):
+def upload(request):
+
+    if request.method == "POST":
+        form = FoodForm(request.POST, request.FILES)
+        form.instance.user = request.user
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = FoodForm()
+    return render(request, 'upload.html', dict(form=form))
+
+
+def get_profile_posts(request):
+
+    if request.method == "GET":
+        user_id = request.GET.get('user')
+        if user_id:
+            foods = Food.objects.filter(user=int(user_id))
+            profile = Profile.objects.filter(user=int(user_id))
+        else:
+            foods = []
+            profile = []
+
+        return render(request, 'profile.html', dict(
+            foods=foods,
+            profile=profile,
+        ))
+def get_all_posts(request):
+    user_id = request.GET.get('user')
+
 
