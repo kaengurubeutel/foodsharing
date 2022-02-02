daa4c564905bbfa803ff969a509e7256db0151cc
Maxi <maximilian.uhlig@hs-augsburg.de>
2022-02-02 17:26:56 +0100

    profile form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register
+from .views import register, createprofile
 
 
 urlpatterns =[
     path('register', register, name='register_url'),
+    path('profile', createprofile, name='profile'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('login', LoginView.as_view(), name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -14,13 +14,20 @@ class ProfileForm(forms.ModelForm):
 def register(request):
     if request.method == 'POST':
         userForm = UserCreationForm(request.POST)
-        #profileForm = ProfileForm(request.POST)
-        if userForm.is_valid(): #and profileForm.is_valid():
+        if userForm.is_valid():
             userForm.save()
-            #profileForm.save()
-            return redirect('login')
+            return redirect('profile')
     else:
         userForm = UserCreationForm()
-        #profileForm = ProfileForm()
-    return render(request, 'auth/register.html', dict(form=userForm)) #, profileForm=profileForm))
+    return render(request, 'auth/register.html', dict(form=userForm))
 
+
+def createprofile(request):
+    if request.method == 'POST':
+        profileform= ProfileForm(request.POST)
+        if profileform.is_valid():
+            profileform.save()
+            return redirect('login')
+        else:
+            profileForm = ProfileForm()
+        return render (request, 'auth/profile.html', dict(form=profileForm))
