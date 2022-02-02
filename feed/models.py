6582e968d21d6d882b9d49aead1bffc710e5a829
Maxi <maximilian.uhlig@hs-augsburg.de>
2022-02-02 10:50:25 +0100

    init

---
+++ feed/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    avatar = models.ImageField("Avatar", upload_to="avatar", blank=True, null= True)
+    hometown = models.CharField('hometown', max_length=20, default="Bielefeld")
+
+
+class Food(models.Model):
+    image = models.ImageField("Img", upload_to="food", blank= True, null= True)
+    description = models.TextField('description',max_length=200, default = " ")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
