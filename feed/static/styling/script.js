994d54ca1955f1b2a45a62170abe28c6cfce7e3a
Anja Zieger <anja.zieger1@hs-augsburg.de>
2022-02-03 19:52:19 +0100

    feed, jQuery bearbeitet

--- feed/static/styling/script.js
+++ feed/static/styling/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 
     $('input').mouseenter(function(){
-        $('.formula').children('form').children('ul').fadeIn(700);
+        $('.formula').children('form').children('ul').slideDown(700);
     });
 
     $('.img').mouseenter(function(){
-        $(this).children('.description').children('img').css('opacity:', '40%');
+        $(this).children('.post').animate({opacity: '0.2'}, 700);
         $(this).children('.description').fadeIn(700);
     });
 
     $('.img').mouseleave(function(){
-        $(this).children('.description').children('img').css('opacity:', '100%')
+        $(this).children('.post').animate({opacity: '1'}, 700);
         $(this).children('.description').fadeOut(700);
-    })
+
+    });
+
 });
